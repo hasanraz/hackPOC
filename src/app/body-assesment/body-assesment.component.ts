de38c297ac02f7edbefbb6b63d6fcb5961c25ff3
hasanraz <hasanraz2009@gmail.com>
2022-08-04 06:50:58 -0700

    updated

--- src/app/body-assesment/body-assesment.component.ts
+++ src/app/body-assesment/body-assesment.component.ts
@@ -60,57 +60,57 @@ export class BodyAssesmentComponent implements OnInit {
    }, 5000);
   }
   setBowelValue() {
-    // if (this.voiceRecognitionService.sentence[0].includes('in continent') || this.voiceRecognitionService.sentence[0].includes('incontinent')) {
-    //   this.bowel.setValue('Is Incontinent');
-    // } else if (this.voiceRecognitionService.sentence[0].includes('continent')) {
-    //   this.bowel.setValue('Is Continent');
-    // } else if (this.voiceRecognitionService.sentence[0].includes('occasional')) {
-    //   this.bowel.setValue('Occasional accident');
-    // } else {
-    //   this.bowel.setValue('Seems normal');
-    // }
+    if (this.voiceRecognitionService.sentence[0].includes('in continent') || this.voiceRecognitionService.sentence[0].includes('incontinent')) {
+      this.bowel.setValue('Is Incontinent');
+    } else if (this.voiceRecognitionService.sentence[0].includes('continent')) {
+      this.bowel.setValue('Is Continent');
+    } else if (this.voiceRecognitionService.sentence[0].includes('occasional')) {
+      this.bowel.setValue('Occasional accident');
+    } else {
+      this.bowel.setValue('Seems normal');
+    }
 
-    this.bowel.setValue('Is Incontinent');
+    
   }
 
   setBladderValue() {
-    // if (this.voiceRecognitionService.sentence[1].includes('in continent') || this.voiceRecognitionService.sentence[1].includes('incontinent')) {
-    //   this.bladder.setValue('Is Incontinent');
-    // } else if (this.voiceRecognitionService.sentence[1].includes('continent')) {
-    //   this.bladder.setValue('Is Continent');
-    // } else if (this.voiceRecognitionService.sentence[1].includes('occasional')) {
-    //   this.bladder.setValue('Occasional accident');
-    // } else {
-    //   this.bladder.setValue('Seems normal');
-    // }
+    if (this.voiceRecognitionService.sentence[1].includes('in continent') || this.voiceRecognitionService.sentence[1].includes('incontinent')) {
+      this.bladder.setValue('Is Incontinent');
+    } else if (this.voiceRecognitionService.sentence[1].includes('continent')) {
+      this.bladder.setValue('Is Continent');
+    } else if (this.voiceRecognitionService.sentence[1].includes('occasional')) {
+      this.bladder.setValue('Occasional accident');
+    } else {
+      this.bladder.setValue('Seems normal');
+    }
 
-    this.bladder.setValue('Occasional accident');
+   
   }
  
   setGroomingValue() {
-    // if (this.voiceRecognitionService.sentence[2].includes('independent')) {
-    //   this.bowel.setValue('Is Independent');
-    // } else if (this.voiceRecognitionService.sentence[2].includes('help') || this.voiceRecognitionService.sentence[2].includes('needs help')) {
-    //   this.bowel.setValue('Needs help');
-    // } else {
-    //   this.bowel.setValue('Seems normal');
-    // }
+    if (this.voiceRecognitionService.sentence[2].includes('independent')) {
+      this.bowel.setValue('Is Independent');
+    } else if (this.voiceRecognitionService.sentence[2].includes('help') || this.voiceRecognitionService.sentence[2].includes('needs help')) {
+      this.bowel.setValue('Needs help');
+    } else {
+      this.bowel.setValue('Seems normal');
+    }
 
-    this.grooming.setValue('Needs help');
+    
   }
 
   setFeedingValue() {
-    // if (this.voiceRecognitionService.sentence[3].includes('independent')) {
-    //   this.feeding.setValue('Is Independent');
-    // } else if (this.voiceRecognitionService.sentence[3].includes('unable')) {
-    //   this.feeding.setValue('Is Unable to feed');
-    // } else if (this.voiceRecognitionService.sentence[3].includes('help') || this.voiceRecognitionService.sentence[3].includes('needs help')) {
-    //   this.feeding.setValue('Needs help');
-    // } else {
-    //   this.feeding.setValue('Seems normal');
-    // }
+    if (this.voiceRecognitionService.sentence[3].includes('independent')) {
+      this.feeding.setValue('Is Independent');
+    } else if (this.voiceRecognitionService.sentence[3].includes('unable')) {
+      this.feeding.setValue('Is Unable to feed');
+    } else if (this.voiceRecognitionService.sentence[3].includes('help') || this.voiceRecognitionService.sentence[3].includes('needs help')) {
+      this.feeding.setValue('Needs help');
+    } else {
+      this.feeding.setValue('Seems normal');
+    }
 
-    this.feeding.setValue('Is Unable to feed');
+   
   }
 
 
